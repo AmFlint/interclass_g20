ba9f7515c2bae565550c1f3f7d04b1179698e819
ThomasGdmc <thomasgrandmagnac@gmail.com>
2018-07-05 17:20:07 +0200

    [ADD/Screen] Delivery step 2

--- app/src/main/java/com/example/interclassg20/cure/Delivery1Activity.kt
+++ app/src/main/java/com/example/interclassg20/cure/Delivery1Activity.kt
@@ -1,22 +1,25 @@
 package com.example.interclassg20.cure
 
 
-import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.BaseAdapter
-import android.widget.TextView
 import androidx.navigation.Navigation
-import kotlinx.android.synthetic.main.fragment_delivery.*
+import kotlinx.android.synthetic.main.fragment_delivery_1.*
 
-class DeliveryActivity : Fragment() {
+
+class Delivery1Activity : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_delivery, container, false)
+        return inflater.inflate(R.layout.fragment_delivery_1, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        pillsBottleImage.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.toDelivery2))
     }
 }
